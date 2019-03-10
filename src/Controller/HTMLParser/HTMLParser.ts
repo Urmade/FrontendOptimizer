eea806cb1d204bfcb1d602f36448975ec7663067
Tobias Urban <turban2009@live.de>
2019-03-10 21:51:16 +0100

    Implemeted function to remove all unneccessary whitespace in closing HTML tags

--- src/Controller/HTMLParser/HTMLParser.ts
+++ src/Controller/HTMLParser/HTMLParser.ts
@@ -2,7 +2,7 @@ import { Element } from "./HTMLElement";
 export class HTMLParser {
     DOM: Element;
     constructor(file: string) {
-        file = this.removeComments(file);
+        file = this.removeClosingTagWhitespace(this.removeComments(file));
         this.DOM = new Element(file);
     }
 
@@ -21,6 +21,15 @@ export class HTMLParser {
         }
         else if (commentStart > -1) {
             return fileString.substring(0, commentStart);
+        }
+        return fileString;
+    }
+    removeClosingTagWhitespace(fileString: string) {
+        //All Closing tags with whitespace in them
+        let ctw = fileString.match(new RegExp("<\\/[a-zA-Z0-9]*\\s+>","ig"));
+        for(let i = 0; i < ctw.length; i++) {
+            let ctwTag = ctw[i].substring(ctw[i].indexOf("/")+1,ctw[i].indexOf(" "));
+            fileString = fileString.replace(ctw[i], `</${ctwTag}>`);
         }
         return fileString;
     }
